4dfaf77079ef5667a322ab81eecb17ac18589159
Kulazar <eneanuch@gmail.com>
2020-12-29 23:34:53 +0300

    very big update

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import pygame
 
 DEBUG = 0
 
-print_logs = 0
-log_it = 0
+print_logs = False
+log_it = False
 
 
 if __name__ == '__main__':
     if DEBUG:
-        print_logs = 1
-        log_it = 1
+        print_logs = True
+        log_it = True
     main_log = MainLog.MainLog(print_logs, log_it)
     fm = FunctionManager.FunctionManager(main_log, CFGManager.CFGManager, DBManager.DBManager,
                                          IMGManager.IMGManager, TranslateManager.TranslateManager)
